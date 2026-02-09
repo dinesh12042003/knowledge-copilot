a76d17456b411ab9be14e56fc6fd4fdee540cfac
Dinesh Paridala <dineshchowdharyparidala@gmail.com>
2026-02-10 01:13:21 +0530

    URL

--- frontend/app.py
+++ frontend/app.py
@@ -10,7 +10,7 @@ from auth.google_auth import login, logout
 # -----------------------------
 # Backend Endpoint
 # -----------------------------
-BACKEND_URL = "https://knowledge-copilot-mo21.onrender.com/"
+BACKEND_URL = "https://knowledge-copilot-mo21.onrender.com"
 
 # -----------------------------
 # Page Configuration
